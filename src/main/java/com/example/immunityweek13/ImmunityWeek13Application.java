04a45242a3b5d92191ea08fda4da42fd945444d7
David Ismael A.M <https://github.com/Rextract/G36_first_lecture.git>
2021-08-24 08:33:13 +0200

    start

---
+++ src/main/java/com/example/immunityweek13/ImmunityWeek13Application.java
@@ -0,0 +1,13 @@
+package com.example.immunityweek13;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImmunityWeek13Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImmunityWeek13Application.class, args);
+    }
+
+}
